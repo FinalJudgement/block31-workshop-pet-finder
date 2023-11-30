33a63ea084c175d935b8817de79e6505948d9a03
Carlos Silva <silvacj90@gmail.com>
2023-11-30 00:12:30 -0500

    all requirements met

--- server.js
+++ server.js
@@ -3,69 +3,71 @@ const app = express();
 
 const pets = require("./data.js");
 
-app.get("/api/v1/pets", (req, res) => {
-  const getAppointments = (aptArr) => {
-    return aptArr
-      .map((apt) => {
-        return `
-        <p>Date: ${apt.date}</p>
-        <p>time: ${apt.time}</p>
-        <p>reason: ${apt.reason}</p>
-      `;
-      })
-      .join();
-  };
-  const petList = pets
-    .map((obj) => {
+const getAppointments = (aptArr) => {
+  return aptArr
+    .map((apt) => {
       return `
-      <div>
-        <h2>${obj.name}</h2>
-        <p>Breed: ${obj.breed}</p>
-        <p>Age: ${obj.age}</p>
-        <p>Owner: ${obj.owner}</p>
-        <p>Telephone: ${obj.telephone}</p>
-        <div>
-        <h3> Appointments: </h3>
-        ${getAppointments(obj.appointments)}
-        </div>
-      </div>
+      <p>Date: ${apt.date}</p>
+      <p>time: ${apt.time}</p>
+      <p>reason: ${apt.reason}</p>
     `;
     })
     .join();
-  res.send(petList);
+};
+
+const populatePetCards = (info) => {
+  return info
+    .map((obj) => {
+      return `
+    <div>
+      <h2>${obj.name}</h2>
+      <p>Breed: ${obj.breed}</p>
+      <p>Age: ${obj.age}</p>
+      <p>Owner: ${obj.owner}</p>
+      <p>Telephone: ${obj.telephone}</p>
+      <div>
+      <h3> Appointments: </h3>
+      ${getAppointments(obj.appointments)}
+      </div>
+    </div>
+  `;
+    })
+    .join();
+};
+
+app.get("/api/v1/pets", (req, res) => {
+  res.send(populatePetCards(pets));
+});
+
+app.get("/api/v1/pets/owner", (req, res) => {
+  const { name } = req.query;
+  const ownersPet = pets.filter(
+    (pet) => pet.owner.toLowerCase() === name.toLowerCase()
+  );
+  console.log(ownersPet);
+
+  if (ownersPet) {
+    res.send(populatePetCards(ownersPet));
+  } else {
+    res.send(`<h3>There is no owner with that name in our database</h3>`);
+  }
 });
 
 app.get("/api/v1/pets/:name", (req, res) => {
-  const getAppointments = (aptArr) => {
-    return aptArr
-      .map((apt) => {
-        return `
-        <p>Date: ${apt.date}</p>
-        <p>time: ${apt.time}</p>
-        <p>reason: ${apt.reason}</p>
-      `;
-      })
-      .join();
-  };
-
-  const nameMatch = () => {
-    return pets.find(
-      (pet) => pet.name.toLowerCase() === req.params.name.toLowerCase()
-    );
-  };
-  console.log(pets);
-  console.log(nameMatch());
-  if (nameMatch()) {
+  const pet = pets.find(
+    (pet) => pet.name.toLowerCase() === req.params.name.toLowerCase()
+  );
+  if (pet) {
     res.send(`
     <div>
-        <h2>${nameMatch().name}</h2>
-        <p>Breed: ${nameMatch().breed}</p>
-        <p>Age: ${nameMatch().age}</p>
-        <p>Owner: ${nameMatch().owner}</p>
-        <p>Telephone: ${nameMatch().telephone}</p>
+        <h2>${pet.name}</h2>
+        <p>Breed: ${pet.breed}</p>
+        <p>Age: ${pet.age}</p>
+        <p>Owner: ${pet.owner}</p>
+        <p>Telephone: ${pet.telephone}</p>
         <div>
         <h3> Appointments: </h3>
-        ${getAppointments(nameMatch().appointments)}
+        ${getAppointments(pet.appointments)}
         </div>
       </div>
     `);
